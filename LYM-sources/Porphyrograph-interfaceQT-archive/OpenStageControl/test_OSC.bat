3bc070c95f1f50201a4a3045620672d545f07f70
Yukao Nagemi <yukao@users.noreply.github.com>
2018-05-17 17:03:49 +0200

    deletion of qt interface

--- LYM-sources/Porphyrograph-interfaceQT-archive/OpenStageControl/test_OSC.bat
+++
@@ -1,1 +0,0 @@
-"D:\Program Files\open-stage-control-0.28.3-win32-x64\open-stage-control.exe"  --send 127.0.0.1:5555 127.0.0.1:6666 --port 7777 --load test_OSC.json --theme light
--- LYM-sources/Porphyrograph-interfaceQT-archive/pg-interface-core-main.h
+++
@@ -1,7 +0,0 @@
-#ifndef PGINTERFACESONGMAIN_H
-#define PGINTERFACESONGMAIN_H
-
-extern MainWindow *pg_window;
-extern QApplication *pg_app;
-
-#endif // PGINTERFACESONGMAIN_H
